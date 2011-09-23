01da8a140379bc2996b4cd0fad6777a5701dff7f
Dexter <dexter.ruby@gmail.com>
2011-09-23 19:52:56 -0400

    fix folder structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '574050aac0ab4875b56c6f74f0ba4cf6a7d8de1fc5f07dca3e8c327086de2b66c031c0f7d0c6e918d041a4c54dcdff1cf240f2f050d7c65a7da4edb6a2f79532'

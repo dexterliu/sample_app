76e6205dba0fd6537d3a61fe381c137c6fbf4d31
Dexter <dexter.ruby@gmail.com>
2011-10-02 17:21:51 -0400

    Added a stub User show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,9 @@ class User < ActiveRecord::Base
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name,  :presence => true, 
-                    :length => { :maximum => 50 }
-  validates :email, :presence => true, 
-                    :format => {:with => email_regex},
+                    :length   => { :maximum => 50 }
+  validates :email, :presence   => true, 
+                    :format     => {:with => email_regex},
                     :uniqueness => {:case_sensitive => false}
 end
 

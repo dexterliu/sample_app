dd3108acd1003c7c4297506b1f77a7d83267a51b
Dexter <dexter.ruby@gmail.com>
2011-10-04 22:56:15 -0400

    Added a signup form

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -45,16 +45,15 @@ describe UsersController do
   end 
             
   describe "GET 'new'" do
+    
     it "should be successful" do
       get :new
       response.should be_success
     end
-  end
   
-  it "should have the right title" do
-    get :new
-    response.should have_selector("title",
-                                  :content => "Sign up")
+    it "should have the right title" do
+      get :new
+    response.should have_selector("title", :content => "Sign up")
+    end
   end
-
 end

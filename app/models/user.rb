031dff4ea3c16f4b407b2c636d4b84bfa716e4c4
Dexter <dexter.ruby@gmail.com>
2011-10-10 01:02:28 -0400

    Working user signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,12 @@ class User < ActiveRecord::Base
   class << self
     def authenticate(email, submitted_password)
       user = find_by_email(email)
-      return nil  if user.nil?
-      return user if user.has_password?(submitted_password)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
+    
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil #ternary operator
     end
   end
     
@@ -48,6 +52,7 @@ end
 
 
 
+
 # == Schema Information
 #
 # Table name: users
@@ -58,5 +63,6 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
